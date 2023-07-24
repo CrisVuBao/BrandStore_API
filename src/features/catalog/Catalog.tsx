18af3cc11464786de9363544ac527a249f0de598
CrisMixi <mobile02022002@gmail.com>
2023-07-24 21:09:31 +0700

    011.2 không sử dụng Catalog(props) | Category({products,addProduct}).

--- src/features/catalog/Catalog.tsx
+++ src/features/catalog/Catalog.tsx
@@ -5,15 +5,15 @@ interface Props {
     addProduct: () => void;
 }
 
-const Catalog = (props : Props) => { // props là parameter ánh xạ đến products,addProduct bên App.tsx, để lấy và sử dụng các thuộc tính của products
+const Catalog = ({products,addProduct}: Props) => { // props là parameter ánh xạ đến products,addProduct bên App.tsx, để lấy và sử dụng các thuộc tính của products
     return ( // this is place generate UI 
         <>
             <ul>
-                {props.products.map((itemProduct) => ( // itemProduct là tham số để tham chiếu đến products, để lấy các thuộc tính, dữ liệu trong products
+                {products.map((itemProduct) => ( // itemProduct là tham số để tham chiếu đến products, để lấy các thuộc tính, dữ liệu trong products
                     <li key={itemProduct.id}>{itemProduct.name} - {itemProduct.price} - {itemProduct.pictureUrl}</li> // dấu "-" là của code html
                 ))}
             </ul>
-            <button onClick={props.addProduct}>Click add product</button>
+            <button onClick={addProduct}>Click add product</button>
         </>
 
     )
