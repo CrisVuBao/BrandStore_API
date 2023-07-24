6aef18f59a37ae2c65196064045eee3ca8681b2f
CrisMixi <mobile02022002@gmail.com>
2023-07-24 09:53:26 +0700

    02 API Basics|| Add Data in table Product (Importaint!!!: Program.cs)

---
+++ BrandStore/Data/StoreContext.cs
@@ -0,0 +1,17 @@
+﻿using BrandStore.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BrandStore.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        #region DbSet
+        public DbSet<Product> Products { get; set; }
+        #endregion
+    }
+}
---
+++ BrandStore/Program.cs
@@ -0,0 +1,44 @@
+﻿using BrandStore.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrandStore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+            // đây là phần di chuyển dữ liệu vào trong SqlServer (Lưu ý: Nếu muốn thêm dữ liệu vào trong bảng Product thì phải Drop-Database in NuGet, rồi Start lại IIS Express thì mới có value trong bảng product)
+            try
+            {
+                context.Database.Migrate();
+                DbInitializer.Initialize(context);
+            }catch (Exception ex)
+            {
+                logger.LogError(ex, "Problem migrating data");
+            }
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
