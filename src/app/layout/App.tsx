534edffc03d49384366ec370e6d7027f65a38abb
CrisMixi <mobile02022002@gmail.com>
2023-07-26 08:59:18 +0700

    006 styles ProductDetails, and add content

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'; // this is React Hook
 import Header from './Header';
 import { Container, CssBaseline, Paper, ThemeProvider, createTheme } from '@mui/material';
 import { Outlet } from 'react-router-dom';
-import Image from '../../img/background.webp';
 
 const App = () => { // Function Components
   const [darkMode, setDarkMode] = useState(false);
@@ -11,7 +10,7 @@ const App = () => { // Function Components
      palette: {
       mode: paletteType, // mode: dùng để điều chỉnh đế độ dark hay là light
       background: {
-        default: paletteType === "light" ? "#FFF0F5" : "#121212"
+        default: paletteType === "light" ? "F6F4EB" : "#121212"
       }
      }
   });
@@ -20,24 +19,15 @@ const handleThemeChange = () => {
   setDarkMode(!darkMode) // darkMode = true (dark)
 }
 
-const styles = {
-  paperContainer: {
-    backgroundImage: `url(${Image})`
-  }
-}
-
   // phần xử lý UI
   return (
     <div className='app'> {/* dấu <> </> là parents dùng để nhúng html bên trong */}
       <ThemeProvider theme={theme}>
         <CssBaseline/> {/*cho phần Header sát đầu web, cho ko còn khoảng trống trên đầu */}
         <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/> {/* đây gọi là handleThemeChange của compo Header = với handle.. của App.tsx */}
-        <Paper style={styles.paperContainer}>
             <Container>
               <Outlet />
             </Container>
-        </Paper>
-
       </ThemeProvider> 
     </div> 
   );
--- src/features/catalog/ProductDetails.tsx
+++ src/features/catalog/ProductDetails.tsx
@@ -1,29 +1,79 @@
-import { Typography } from "@mui/material";
+import {
+    Divider,
+    Grid,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableRow,
+    Typography,
+} from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import axios from "axios"
+import axios from "axios";
 import { Product } from "../../app/models/product";
 
 const ProductDetails = () => {
-    const {id} = useParams<{id: string}>();
+    const { id } = useParams<{ id: string }>();
     const [itemProduct, setProduct] = useState<Product | null>(null); // truyền model Product vào trong useState để quản lý sản phẩm
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        axios.get(`https://localhost:44386/api/Products/${id}`)
-            .then(Response => setProduct(Response.data))
-            .catch(error => console.log(error))
+        axios
+            .get(`https://localhost:44386/api/Products/${id}`)
+            .then((Response) => setProduct(Response.data))
+            .catch((error) => console.log(error))
             .finally(() => setLoading(false)); // cuối cùng nếu lỗi thì là false
-    }, [id])
+    }, [id]);
 
-    if(loading) return <h5>..loading</h5>
-    if (!itemProduct) return <h3>not product</h3>
+    if (loading) return <h5>..loading</h5>;
+    if (!itemProduct) return <h3>not product</h3>;
 
     return (
-        <Typography variant="h1">
-            {itemProduct.name}
-        </Typography>
-    )
-}
+        <Grid container spacing={6}>
+            <Grid item xs={6}>
+                <img
+                    src={itemProduct.pictureUrl}
+                    alt={itemProduct.name}
+                    style={{ width: "100%" }}
+                />
+            </Grid>
+            <Grid item xs={6}>
+                <Typography variant="h3">{itemProduct.name}</Typography>
+                <Divider sx={{ mb: 2 }} />{" "}  {/*tạo 1 dải đường nhỏ, gạch chân bên dưới content */}   
+                <Typography variant="h4" color="primary">
+                    {(itemProduct.price / 100).toFixed(3)} VNĐ
+                </Typography>
+                {/*Bên dưới là phần Table thông tin product */}
+                <TableContainer>
+                    <Table>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell>{itemProduct.name}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Description</TableCell>
+                                <TableCell>{itemProduct.description}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Type</TableCell>
+                                <TableCell>{itemProduct.type}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Brand</TableCell>
+                                <TableCell>{itemProduct.brand}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Quantity in stock</TableCell>
+                                <TableCell>{itemProduct.quantityInStock}</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Grid>
+        </Grid>
+    );
+};
 
 export default ProductDetails;
