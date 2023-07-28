a43365473b12a6869ea0918f634b2f294b00d54f
CrisMixi <mobile02022002@gmail.com>
2023-07-28 15:04:59 +0700

    Nếu có item trong giỏ hàng rồi thì Button là Update Quantity, chưa có item thì ADD TO CART (ProductDetails.tsx)

--- src/features/catalog/ProductDetails.tsx
+++ src/features/catalog/ProductDetails.tsx
@@ -1,4 +1,4 @@
-import { Divider,Grid,Table,TableBody,TableCell,TableContainer,TableRow,Typography,} from "@mui/material";
+import { Divider,Grid,Table,TableBody,TableCell,TableContainer,TableRow,TextField,Typography,} from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/product";
@@ -6,18 +6,25 @@ import agent from "../../app/api/agent";
 import NotFound from "../../app/errors/NotFound";
 import LoadingComponent from "../../app/layout/LoadingComponent";
 import { currencyFormat } from "../../app/util/util";
+import { useStoreContext } from "../../app/context/StoreContext";
+import { LoadingButton } from "@mui/lab";
 
 export default function ProductDetails() {
+    const {basket} = useStoreContext(); // lấy data, properties của Basket
     const { id } = useParams<{ id: string}>(); // usePrams thì truyền tham số vào phải là kiểu string, ko truyền kiểu number vào được (vì gốc của nó là kiểu string)
     const [itemProduct, setProduct] = useState<Product | null>(null); // truyền model Product vào trong useState để quản lý sản phẩm
     const [loading, setLoading] = useState(true);
+    const [quantity, setQuantity] = useState(0);
+    const [submitting, setSubmitting] = useState(false);
+    const item = basket?.items.find(i => i.productId === itemProduct?.id); // so sánh xem productId của Product có bằng productId của Basket hay không
 
     useEffect(() => {
+        if (item) setQuantity(item.quantity); // thêm số lượng(quantity) từ ProductDetails vào trong Basket
         id && agent.Catalog.details(parseInt(id))
             .then((Response) => setProduct(Response))
             .catch((error) => console.log(error.Response))
             .finally(() => setLoading(false)); // cuối cùng nếu lỗi thì là false
-    }, [id]);
+    }, [id, item]);
 
     if (loading) return <LoadingComponent />;
     if (!itemProduct) return <NotFound />; // nếu id sản phẩm ko có, thì sẽ chuyển qua NotFound
@@ -64,6 +71,28 @@ export default function ProductDetails() {
                         </TableBody>
                     </Table>
                 </TableContainer>
+                <Grid container spacing={2}>
+                    <Grid item xs={6}> {/*xs: là muốn truyền bao nhiều số cột bên trong, như đây là Grid sẽ lấy 6 cột trong 12 cột */}
+                        <TextField 
+                            variant="outlined"
+                            type="number"
+                            label='Quantity in Cart'
+                            fullWidth
+                            value={quantity}
+                        />
+                    </Grid>
+                    <Grid item xs={6}> {/*xs: là muốn truyền bao nhiều số cột bên trong, như đây là Grid sẽ lấy 6 cột trong 12 cột */}
+                        <LoadingButton
+                            sx={{height: '55px'}}
+                            color="primary"
+                            size="large"
+                            variant="contained"
+                            fullWidth
+                        >
+                            {item ? 'Update Quantity' : 'Add to Cart'}
+                        </LoadingButton>
+                    </Grid>
+                </Grid>
             </Grid>
         </Grid>
     );
